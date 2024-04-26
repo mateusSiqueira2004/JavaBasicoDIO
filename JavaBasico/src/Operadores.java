839dae3e5041809cd13bbd235abb4155d0bd1689
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-04-26 14:24:45 -0300

    Operadores

--- JavaBasico/src/Operadores.java
+++ JavaBasico/src/Operadores.java
@@ -64,5 +64,29 @@ public class Operadores {
         String resultado = a == b ? "verdadeiro" : "falso";
         
         System.out.println(resultado);
+
+        System.out.println("\n------------------\nOperadores\n\n");
+        int numero1 = 1, numero2 = 2; 
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numero1 é igual a numero2?\n" + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numero1 é diferente a numero2?\n" + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numero1 é maior a numero2?\n" + simNao);
+
+        if(numero1 < numero2){
+            System.out.println("numero1 é menor que numero2");
+        }
+
+        String nome1 = "Mateus";
+        String nome2 = new String("Mateus");
+
+        System.out.println(nome1.equals(nome2));
     }
 }
