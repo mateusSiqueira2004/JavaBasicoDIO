18d3a20c2bebe369b70fa828067295337a89855c
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-04-26 13:57:25 -0300

    Unarios

--- JavaBasico/src/Operadores.java
+++ JavaBasico/src/Operadores.java
@@ -26,5 +26,19 @@ public class Operadores {
         concatenacao = "1"+(1+1+1);
 
         System.out.println(concatenacao);
+
+        int numero = 5;
+
+        System.out.println(- numero);
+        System.out.println(numero);
+
+        numero =- numero;
+        
+        System.out.println(numero);
+
+        numero = numero *-1;
+
+        System.out.println(numero);
+        
     }
 }
