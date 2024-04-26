e77ac805e2a3ea989196c620e7670fe2ba319882
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-04-26 14:03:16 -0300

    Aritmética

--- JavaBasico/src/Operadores.java
+++ JavaBasico/src/Operadores.java
@@ -33,12 +33,26 @@ public class Operadores {
         System.out.println(numero);
 
         numero =- numero;
-        
+
         System.out.println(numero);
 
         numero = numero *-1;
 
         System.out.println(numero);
         
+        numero = numero + 2;
+
+        System.out.println(numero);
+
+        System.out.println( ++ numero);
+
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+
     }
 }
