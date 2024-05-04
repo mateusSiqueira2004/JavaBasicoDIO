ff462d8064490c4c94aa3c1db85d015409a8230a
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-05-04 01:22:57 -0300

    CriandoExcecao

---
+++ JavaBasico/src/excecao/CepInvalidoException.java
@@ -0,0 +1,3 @@
+package excecao;
+
+public class CepInvalidoException extends Exception{}
---
+++ JavaBasico/src/excecao/FormatadorCep.java
@@ -0,0 +1,17 @@
+package excecao;
+public class FormatadorCep{
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("23765064");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException ex) {
+            System.out.println("cep inválido");
+        }
+    }
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if(cep.length() != 8)
+            throw new CepInvalidoException();
+
+        return "23.765-064";
+    }
+}
