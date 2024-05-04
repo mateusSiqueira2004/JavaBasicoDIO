b0ecc836a70af4bbc45ff1fd75903ffad8e56e92
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-05-04 00:28:40 -0300

    ForArrays

---
+++ JavaBasico/src/lacos_repeticao/ExemploForArray.java
@@ -0,0 +1,12 @@
+package lacos_repeticao;
+
+public class ExemploForArray{
+    public static void main(String[] args) {
+        String alunos[] = {"Felipe", "Jonas", "Julia", "Marcos"};
+        for( String aluno : alunos){
+            System.out.println("Nome do aluno e " + aluno);
+        }
+        
+    }
+
+}
