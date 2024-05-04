eb26a976a34e7bec012e54df16dd235122f91a74
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-05-04 00:47:57 -0300

    ExemploDoWhile

---
+++ JavaBasico/src/lacos_repeticao/ExemploDoWhile.java
@@ -0,0 +1,23 @@
+package lacos_repeticao;
+
+import java.util.Random;
+
+public class ExemploDoWhile{
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+
+        do{
+            System.out.println("Telefone tocando");
+
+        }while(tocando());
+
+        System.out.println("Alo!");
+    }
+
+    private static boolean tocando(){
+        boolean atendeu = new Random().nextInt(3)==1;
+        System.out.println("Atendeu? " + atendeu);
+        return ! atendeu;
+    }
+
+}
