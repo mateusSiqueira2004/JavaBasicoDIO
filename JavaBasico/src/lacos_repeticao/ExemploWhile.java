8e7e27d8aef90bb3b38d4a4c8f45ad2a312158ce
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-05-04 00:41:13 -0300

    ExemploWhile

---
+++ JavaBasico/src/lacos_repeticao/ExemploWhile.java
@@ -0,0 +1,23 @@
+package lacos_repeticao;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ExemploWhile{
+    public static void main(String[] args) {
+        double mesada = 50.0;
+        while (mesada>0){
+            Double valorDoce = valorAleatorio();
+            if(valorDoce > mesada)
+                valorDoce = mesada;
+
+            System.out.println("Doce do valor: " + valorDoce);
+            mesada = mesada - valorDoce;
+        }
+        System.out.println("Mesada: "+ mesada);
+        System.out.println("Gastou toda a mesada em doces ");
+    }
+    private static double valorAleatorio(){
+        return ThreadLocalRandom.current().nextDouble(2, 8);
+    }
+
+}
