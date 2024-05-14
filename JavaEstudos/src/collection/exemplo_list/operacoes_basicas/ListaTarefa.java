7f5c175e407e3c33588ba318eaaf4c4ea2ba8b85
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-05-14 17:48:43 -0300

    ListBasico

---
+++ JavaEstudos/src/collection/exemplo_list/operacoes_basicas/ListaTarefa.java
@@ -0,0 +1,32 @@
+package collection.exemplo_list.operacoes_basicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa(){
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasRemover = new ArrayList<>();
+
+        for(Tarefa t : tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasRemover);
+    }
+    public int numeroTotalTarefas(){
+        return tarefaList.size();
+    }
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+}
---
+++ JavaEstudos/src/collection/exemplo_list/operacoes_basicas/Tarefa.java
@@ -0,0 +1,13 @@
+package collection.exemplo_list.operacoes_basicas;
+
+public class Tarefa {
+    private String descricao;
+
+    public Tarefa(String descricao){
+        this.descricao = descricao;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+}
