eadb42f755affac53d45c8561b6fb0a1631f7cfa
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-05-15 18:14:23 -0300

    Ordenacao Pronta

---
+++ JavaEstudos/src/collection/exemplo_setInterface/ordenacao/CadastroProduto.java
@@ -0,0 +1,29 @@
+package collection.exemplo_setInterface.ordenacao;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class CadastroProduto {
+    private Set<Produto> produtoSet;
+
+    public CadastroProduto() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(String nome, long codigo, double preco, int quantidade){
+        produtoSet.add(new Produto(nome, codigo, preco, quantidade));
+    }
+    public Set<Produto> exibirProdutoPorNome(){
+        Set<Produto> produtoPorNome = new TreeSet<>(produtoSet);
+
+        return produtoPorNome;
+    }
+    public Set<Produto> exibirProdutoPorPreco(){
+        Set<Produto> produtoPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtoPorPreco.addAll(produtoSet);
+        return produtoPorPreco;
+    }
+
+    
+}
