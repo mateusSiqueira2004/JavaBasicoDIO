545872042a3b2b7c8e45d3b4efa02023030ba332
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-05-15 16:57:08 -0300

    testes realizados

--- JavaEstudos/src/collection/exemplo_list/ordenacao/OrdenacaoPessoa.java
+++ JavaEstudos/src/collection/exemplo_list/ordenacao/OrdenacaoPessoa.java
@@ -31,6 +31,19 @@ public class OrdenacaoPessoa {
 
         return pessoasPorAltura;
     }
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
 
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 20, 1.76);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 30, 1.60);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 35, 1.80);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 17, 1.89);
+        ordenacaoPessoa.adicionarPessoa("Nome 5", 15, 1.56);
+        ordenacaoPessoa.adicionarPessoa("Nome 6", 60, 1.60);
+
+        System.out.println("ordenando por Idade: "+ordenacaoPessoa.ordenarPorIdade());
+
+        System.out.println("ordenando por Altura: "+ordenacaoPessoa.ordenarPorAltura());
+    }
     
 }
