966757aecd7af0e282e4b8038b77fce0cfa32b3c
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-05-16 17:13:28 -0300

    testesRealizados

--- JavaEstudos/src/collection/exemplo_map/pesquisa/EstoqueProduto.java
+++ JavaEstudos/src/collection/exemplo_map/pesquisa/EstoqueProduto.java
@@ -10,7 +10,7 @@ public class EstoqueProduto {
         this.estoqueProdutoMap = new HashMap<>();
     }
 
-    public void adicionarProduto(Long cod, String nome, int quantidade, double preco){
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco){
         estoqueProdutoMap.put(cod, new Produto(nome, preco, quantidade));
     }
 
@@ -39,5 +39,22 @@ public class EstoqueProduto {
             }
         }
         return produtoMaisCaro;
+    }
+
+    public static void main(String[] args) {
+        EstoqueProduto estoqueProduto = new EstoqueProduto();
+
+        estoqueProduto.adicionarProduto(123, "nome 1", 30, 15.0);
+        estoqueProduto.adicionarProduto(124, "nome 2", 20, 20.0);
+        estoqueProduto.adicionarProduto(125, "nome 3", 60, 19.99);
+        estoqueProduto.adicionarProduto(126, "nome 4", 35, 20.50);
+
+        estoqueProduto.exibirProduto();
+
+        String valorFormatado = String.format("%.2f", estoqueProduto.calculaValorTotalEstoque());
+        System.out.println(valorFormatado);
+
+        System.out.println(estoqueProduto.obterProdutoMaisCaro());
+        
     }
 }
