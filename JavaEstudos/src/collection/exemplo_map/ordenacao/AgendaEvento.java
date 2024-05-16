268a2af0bacfbaed6b6ed0d6a64e25f5f844147a
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-05-16 17:40:46 -0300

    testes realizados

--- JavaEstudos/src/collection/exemplo_map/ordenacao/AgendaEvento.java
+++ JavaEstudos/src/collection/exemplo_map/ordenacao/AgendaEvento.java
@@ -20,11 +20,23 @@ public class AgendaEvento {
     public void obterProximoEvento(){
        LocalDate dataAtual = LocalDate.now();
        Map<LocalDate, Evento> eventoOrdenado = new TreeMap<>(eventoMap);
-       for (Map.Entry<LocalDate, Evento> entry : eventoMap.entrySet()) {
+       for (Map.Entry<LocalDate, Evento> entry : eventoOrdenado.entrySet()) {
            if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)){
                 System.out.println("O proximo evento: "+ entry.getValue() + " acontecera na data " + entry.getKey());
                 break;
            }
        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEvento agendaEvento = new AgendaEvento();
+
+        agendaEvento.adicionarEvento(LocalDate.of(2024, 12, 10), "nome 1", "atracao 1");
+        agendaEvento.adicionarEvento(LocalDate.of(2024, 11, 30), "nome 2", "atracao 2");
+        agendaEvento.adicionarEvento(LocalDate.of(2024, 12, 15), "nome 3", "atracao 3");
+
+        agendaEvento.exibirAgenda();
+
+        agendaEvento.obterProximoEvento();
     }
 }
