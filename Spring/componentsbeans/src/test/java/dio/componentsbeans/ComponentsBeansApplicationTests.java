38c26a91431ba6da0948932339479e175e7aa643
mateusSiqueira <mateussiqueirasalomao@gmail.com>
2024-06-25 16:49:32 -0300

    Beans

---
+++ Spring/componentsbeans/src/test/java/dio/componentsbeans/ComponentsBeansApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.componentsbeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentsBeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
